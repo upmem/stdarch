b112564ccb70b56e3c53a8be771de60c67f9f588
Jordi Chauzi <jchauzi@upmem.com>
2020-08-10 09:52:13 +0200

    dpu: add dma intrinsics

--- crates/core_arch/src/dpu/mod.rs
+++ crates/core_arch/src/dpu/mod.rs
@@ -3,9 +3,23 @@
 extern "C" {
     #[link_name = "llvm.dpu.tid.i32"]
     fn llvm_thread_id() -> i32;
+    #[link_name = "llvm.dpu.ldma.unchecked"]
+    fn llvm_mram_read(wram: *mut u8, mram: *const u8, len: u32);
+    #[link_name = "llvm.dpu.sdma.unchecked"]
+    fn llvm_mram_write(wram: *const u8, mram: *mut u8, len: u32);
 }
 
 #[inline]
 pub unsafe fn thread_id() -> i32 {
     llvm_thread_id()
+}
+
+#[inline]
+pub unsafe fn mram_read(wram: *mut u8, mram: *const u8, len: usize) {
+    llvm_mram_read(wram, mram, len as u32)
+}
+
+#[inline]
+pub unsafe fn mram_write(wram: *const u8, mram: *mut u8, len: usize) {
+    llvm_mram_write(wram, mram, len as u32)
 }
